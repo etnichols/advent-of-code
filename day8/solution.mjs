414418eb8102d82509ac585fa6484aad4e9bca5d
Evan Thomas Nichols <etnichols@evans-mbp.myfiosgateway.com>
2021-12-19 22:22:16 -0500

    clean up day 8

--- day8/solution.mjs
+++ day8/solution.mjs
@@ -23,7 +23,7 @@ const PATTERN_TO_NUMBER = new Map([
   ['abcdfg', '9'],
 ])
 
-const processLines = async () => {
+async function processLines {
   const filestream = fs.createReadStream(`./input.txt`)
   const rl = readline.createInterface({
     input: filestream,
@@ -31,65 +31,13 @@ const processLines = async () => {
   })
 
   let sum = 0
-
+  
   for await (const line of rl) {
     let [input, output] = line
-      .split(' | ')
-      .map((piece) => piece.split(' ').map((sequence) => sequence.split('')))
-    input.sort((a, b) => a.length - b.length)
-
-    const decoder = new Map()
-    const mustBeA = solveForA(input[0], input[1])
-    decoder.set(mustBeA, 'a')
-
-    let segmentTracker = {}
-    SEGMENT_ARRAY.forEach((char) => {
-      segmentTracker[char] = 0
-    })
-
-    input.forEach((pattern) => {
-      pattern.forEach((character) => {
-        // Skip segment A mapping, to maintain unique count for segment C.
-        if (character !== mustBeA) {
-          segmentTracker[character]++
-        }
-      })
-    })
-
-    // Use unique segment counts to identify mappings.
-    for (const [char, count] of Object.entries(segmentTracker)) {
-      switch (count) {
-        case 6:
-          decoder.set(char, 'b')
-          break
-        case 8:
-          decoder.set(char, 'c')
-          break
-        case 4:
-          decoder.set(char, 'e')
-          break
-        case 9:
-          decoder.set(char, 'f')
-          break
-      }
-    }
-
-    // Solve D and G by process of elimination. Get pattern with length four.
-    // The only unsolved character here must map to segment D.
-    const mustBeD = getOnlyElement(
-      input[2].filter((character) => decoder.get(character) === undefined)
-    )
-    decoder.set(mustBeD, 'd')
-    const mustBeG = getOnlyElement(
-      SEGMENT_ARRAY.filter((character) => decoder.get(character) === undefined)
-    )
-    decoder.set(mustBeG, 'g')
-
-    // Verify.
-    if (decoder.size !== 7) {
-      throw new Error('Decoder not complete')
-    }
-
+    .split(' | ')
+    .map((piece) => piece.split(' ').map((sequence) => sequence.split('')))
+    
+    const decoder = createDecoder(input)
     const decoded = output
       .map(
         (pattern) =>
@@ -102,16 +50,73 @@ const processLines = async () => {
           )
       )
       .join('')
-
-    sum += parseInt(decodedNumber, 10)
+    sum += parseInt(decoded, 10)
   }
 
   console.log('Sum: ', sum)
 }
 
-// A can always be solved using array corresponding to number 1 (length 2)
+function createDecoderMap(input){
+  input.sort((a, b) => a.length - b.length)
+
+  const decoder = new Map()
+  const mustBeA = solveForA(input[0], input[1])
+  decoder.set(mustBeA, 'a')
+
+  let segmentTracker = {}
+  SEGMENT_ARRAY.forEach((char) => {
+    segmentTracker[char] = 0
+  })
+
+  input.forEach((pattern) => {
+    pattern.forEach((character) => {
+      // Skip segment A mapping, to maintain unique count for segment C.
+      if (character !== mustBeA) {
+        segmentTracker[character]++
+      }
+    })
+  })
+
+  // Use unique segment counts to identify mappings.
+  for (const [char, count] of Object.entries(segmentTracker)) {
+    switch (count) {
+      case 6:
+        decoder.set(char, 'b')
+        break
+      case 8:
+        decoder.set(char, 'c')
+        break
+      case 4:
+        decoder.set(char, 'e')
+        break
+      case 9:
+        decoder.set(char, 'f')
+        break
+    }
+  }
+
+  // Solve D and G by process of elimination. Get pattern with length four.
+  // The only unsolved character here must map to segment D.
+  const mustBeD = getOnlyElement(
+    input[2].filter((character) => decoder.get(character) === undefined)
+  )
+  decoder.set(mustBeD, 'd')
+  const mustBeG = getOnlyElement(
+    SEGMENT_ARRAY.filter((character) => decoder.get(character) === undefined)
+  )
+  decoder.set(mustBeG, 'g')
+
+  // Verify.
+  if (decoder.size !== 7) {
+    throw new Error('Decoder not complete')
+  }
+
+  return decoder
+}
+
+// Segment A can always be solved using array corresponding to number 1 (length 2)
 // and number 7 (length 3), as they share 2/3 segments. "Odd one out" must be segment A.
-const solveForA = (mustBeOne, mustBeSeven) => {
+function solveForA(mustBeOne, mustBeSeven){
   if (mustBeOne.length !== 2)
     throw new Error('Expected one of length two but got: ', mustBeOne)
   if (mustBeSeven.length !== 3)
@@ -119,7 +124,7 @@ const solveForA = (mustBeOne, mustBeSeven) => {
   return mustBeSeven.filter((char) => !mustBeOne.includes(char))[0]
 }
 
-const getOnlyElement = (arr) => {
+function getOnlyElement(arr){
   if (arr.length !== 1) {
     throw new Error('More than one element in: ', arr)
   }
